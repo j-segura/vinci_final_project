0a9a734a0c94f953fc4dbd130c001adfa52df7e9
Juan David Segura Zabala <juanda.segura2005@gmail.com>
2024-02-20 20:18:56 -0500

    feat(home_page): working in welcome design

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,15 +4,148 @@
     <AuthenticatedLayout>
         <div class="header">
             <div class="header-img">
-                <img :src="imagenUrl" alt="">
+                <img src="./../img/header-bg.jpg" alt="">
             </div>
             <div class="header-content">
-                <h1>Find your favorite artiste</h1>
-                <input type="search" placeholder="Search...">
+                <h1>Find your favorite artist</h1>
+                <div class="search-bar">
+                    <input type="search" placeholder="Search for someone...">
+                    <button></button>
+                </div>
                 <p>More than 1000 artists, designers & creative people</p>
             </div>
-            {{ imagenUrl }}
         </div>
+        <div class="sub-title">
+            <h2>Look for a Category</h2>
+            <p>Here you will find all the categories we have available</p>
+        </div>
+        <section id="categories">
+            <div>
+                <img src="./../img/digital.jpg" alt="category">
+                <span>Digital Art</span>
+            </div>
+            <div>
+                <img src="./../img/header-bg.jpg" alt="category">
+                <span>Trend</span>
+            </div>
+            <div>
+                <img src="./../img/3d.jpg" alt="category">
+                <span>Abstract</span>
+            </div>
+            <div>
+                <img src="./../img/scullture.jpg" alt="category">
+                <span>Sculptures</span>
+            </div>
+            <div>
+                <img src="./../img/old.jpg" alt="category">
+                <span>Contemporary</span>
+            </div>
+        </section>
+        <div class="sub-title">
+            <h2 class="text-center">Choose from the best</h2>
+            <p class="text-center">Looking for a good artist? find him below</p>
+        </div>
+        <section id="works-index">
+            <div class="work-card">
+                <div class="user-info">
+                    <div class="user-picture"></div>
+                    <span>Daniel Murth | Artist</span>
+                </div>
+                <div class="work">
+                    <div class="img">
+                        <img src="./../img/old.jpg" alt="">
+                    </div>
+                    <div class="work-info">
+                        29 likes
+                    </div>
+                </div>
+            </div>
+            <div class="work-card">
+                <div class="user-info">
+                    <div class="user-picture"></div>
+                    <span>Daniel Murth | Artist</span>
+                </div>
+                <div class="work">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <div class="work-info">
+                        29 likes
+                    </div>
+                </div>
+            </div>
+            <div class="work-card">
+                <div class="user-info">
+                    <div class="user-picture"></div>
+                    <span>Daniel Murth | Artist</span>
+                </div>
+                <div class="work">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <div class="work-info">
+                        29 likes
+                    </div>
+                </div>
+            </div>
+            <div class="work-card">
+                <div class="user-info">
+                    <div class="user-picture"></div>
+                    <span>Daniel Murth | Artist</span>
+                </div>
+                <div class="work">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <div class="work-info">
+                        29 likes
+                    </div>
+                </div>
+            </div>
+            <div class="work-card">
+                <div class="user-info">
+                    <div class="user-picture"></div>
+                    <span>Daniel Murth | Artist</span>
+                </div>
+                <div class="work">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <div class="work-info">
+                        29 likes
+                    </div>
+                </div>
+            </div>
+            <div class="work-card">
+                <div class="user-info">
+                    <div class="user-picture"></div>
+                    <span>Daniel Murth | Artist</span>
+                </div>
+                <div class="work">
+                    <div class="img">
+                        <img src="" alt="">
+                    </div>
+                    <div class="work-info">
+                        29 likes
+                    </div>
+                </div>
+            </div>
+            <div class="work-card">
+                <div class="user-info">
+                    <div class="user-picture"></div>
+                    <span>Daniel Murth | Artist</span>
+                </div>
+                <div class="work">
+                    <div class="img">
+                        <img src="./../img/header-bg.jpg" alt="">
+                    </div>
+                    <div class="work-info">
+                        29 likes
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer></footer>
     </AuthenticatedLayout>
 </template>
 
@@ -33,8 +166,6 @@ export default {
     data() {
 
         return {
-
-        imagenUrl: `${this.$page.props.appUrl}/imagenes/imagen.jpg`
 
         };
 
